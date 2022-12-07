15ea65a8a73eb34577fd45b8a7f45047c4fa7de3
Nihilist-decadent <85698612+Nihilist-decadent@users.noreply.github.com>
2022-12-07 23:36:33 +0300

    Well...

--- Class1.cs
+++ Class1.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.IO;
 using HW_1_Andrey;
 using static System.Net.Mime.MediaTypeNames;
+using System.Reflection;
 
 namespace HW_1_Andrey
 {
@@ -15,12 +16,12 @@ namespace HW_1_Andrey
         private double _Width;
         private double _Height;
 
-        public double Square()
+        public double getSquare()
         {
             return _Width * _Height;
         }
 
-        public double Perimeter()
+        public double getPerimeter()
         {
             return 2 * (_Width + _Height);
         }
@@ -37,105 +38,89 @@ namespace HW_1_Andrey
             set { _Height = value; } 
         }
 
-        public void Print(double square, double perimeter)
+    }
+    internal class RectrainglePrinter
+    {
+        public void Print()
         {
+            Rectrangle rectrangle = new Rectrangle();
+
             Console.WriteLine("inf about tringle:");
-            Console.WriteLine($"wysota: {_Height}");
-            Console.WriteLine($"shyrina: {_Width}");
-            Console.WriteLine($"inf about Square: {Square()}");
-            Console.WriteLine($"inf about Perimeter: {Perimeter()}");
+            Console.WriteLine($"wysota: {rectrangle.Set_height}");
+            Console.WriteLine($"shyrina: {rectrangle.Set_width}");
+            Console.WriteLine($"inf about Square: {rectrangle.getSquare()}");
+            Console.WriteLine($"inf about Perimeter: {rectrangle.getPerimeter()}");
+        }
+    }
+    class RecReadFile
+    {
+        public async void Record()
+        {
+            string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"Data\Names.txt");
+            string[] InfArray = new string[2];
+            Console.WriteLine("input ur width");
+            InfArray[0] = Console.ReadLine();
+
+            Console.WriteLine("input ur height");
+            InfArray[1] = Console.ReadLine();
+
+            using (FileStream fstream = new FileStream(path, FileMode.OpenOrCreate))
+            {
+                for (int i = 0; i < InfArray.Length; i++)
+                {
+                    // преобразуем строку в байты
+                    byte[] buffer = Encoding.Default.GetBytes(InfArray[i]);
+                    // запись массива байтов в файл
+                    await fstream.WriteAsync(buffer, 0, buffer.Length);
+                    Console.WriteLine("Текст записан в файл");
+                }
+            }
+
         }
 
-
+        static public string[] Read()
+        {
+            string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"Data\Names.txt");
+            string[] files = File.ReadAllLines(path);
+            return files;
+        }
+       
     }
     internal class Program
     {
-        //static public int SetWidth()
-        //{
-        //    int result;
-        //    Console.WriteLine("введите ширину");
-        //    while (!int.TryParse(Console.ReadLine(), out result))
-        //    {
-        //        Console.Write("ОШИБКА! Повторите ввод: ");
-        //    }
-        //    return result;
-        //}
-        //static public int SetHeight()
-        //{
-        //    int result;
-        //    Console.WriteLine("ввдите высоту");
-        //    while (!int.TryParse(Console.ReadLine(), out result))
-        //    {
-        //        Console.Write("ОШИБКА! Повторите ввод: ");
-        //    }
-        //    return result;
-        //}
-
         static public int[] Output()
         {
-            StreamReader file = new StreamReader("E:\\info.txt");
-            string OutText = file.ReadLine();
-            int[] ReturnIntValue = new int[3];
+            string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"Data\Names.txt");
+            string[] files = File.ReadAllLines(path);
+            int[] ReturnIntValue = new int[2];
             while (true)
             {
-                if (OutText != null)
+                if (files != null)
                 {
-                    while (true)
-                    {
-                        string[] Words = OutText.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-                        foreach (string SeparateWords in Words)
-                        {
-                        }
-                        if (Words.Length == 2)
-                        {
-                            int[] ValueInt = Array.ConvertAll(Words, SeparateWords => int.Parse(SeparateWords));
-                            ReturnIntValue = ValueInt;
-                        }
-                        else { Console.WriteLine("int 2 chisla dolbaeb"); }
-                        break;
-                    }
+                   if (files.Length == 2)
+                   {
+                      int[] ValueInt = Array.ConvertAll(files, SeparateWords => int.Parse(SeparateWords));
+                      ReturnIntValue = ValueInt;
+                   }
+                   else { Console.WriteLine("int 2 chisla dolbaeb"); }
+                   break;
                 }
                 else { Console.WriteLine("Dolbayeb int something"); }
                 break;
             }
-            file.Close();
             return ReturnIntValue;
         }
-        //static public int[] Convert()
-        //{
-        //    string[] blyt = Output();
-        //    string text ="1";
-        //    for(int i=0; i<blyt.Length; i++)
-        //    {
-        //        if (blyt[i] != null)
-        //        {
-        //            text = blyt[i];
-        //        }
-        //    }
-        //    string[] words = text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-        //    foreach (string s in words)
-        //    {
-        //    }
-
-        //    int[] ValueInt = Array.ConvertAll(words, s => int.Parse(s));
-        //    return ValueInt;
-        //}
         static void Main(string[] args)
-        
         {
             Rectrangle rectrangle = new Rectrangle();
-
+            RectrainglePrinter rectrainglePrinter = new RectrainglePrinter();
+            RecReadFile recread = new RecReadFile();
+            
             int[] OutputValues = Output();
             rectrangle.Set_width = OutputValues[0];
             rectrangle.Set_height = OutputValues[1];
 
-            rectrangle.Print(rectrangle.Square(), rectrangle.Perimeter());
-
-            //StreamWriter file = new StreamWriter("E:\\info.txt", false);
-            //file.WriteLine($"рассчитанная площадь: " + $"{rectrangle.Square()}");
-            //file.WriteLine($"рассчитанный периметр: {rectrangle.Perimeter()}");
-            //file.Close();
-
+            rectrainglePrinter.Print();
             Console.ReadKey();
         }
     }
